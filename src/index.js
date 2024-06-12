587931220df4d5bd423256ff5413f6dbb07de308
xin053 <13207130066.cool@163.com>
2024-06-12 22:20:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ addEventListener("fetch", (event) => {
 // };
 
 const routes = {
-  "${workername}.${username}.workers.dev/": "https://registry-1.docker.io",
+  "cloudflare-docker-proxy.alomt.workers.dev/": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
